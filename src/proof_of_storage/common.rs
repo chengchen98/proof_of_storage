ae5e29aee347a826d1d9d73bc0b930a5f83f0b8d
chengchen98 <interestingcc666@163.com>
2023-03-09 21:09:49 +0800

    modify seal and unseal

---
+++ src/proof_of_storage/common.rs
@@ -0,0 +1,99 @@
+use rand::Rng;
+use std::fs::{File, OpenOptions};
+use std::io::{Seek, Read, SeekFrom, Write};
+
+use super::depend::{long_depend, short_depend};
+use super::data_seal::{DATA_L, L2, L1};
+
+pub fn create_random_file(path: &str) -> std::io::Result<()> {
+    //! 随机创建长度为 DATA_L 字节的文件
+    let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .truncate(true)
+    .open(path)
+    .unwrap();
+
+    let mut rng = rand::thread_rng();
+    for _ in 0..DATA_L {
+        let buf: [u8; 1] = [rng.gen_range(0u8..=255u8)];
+        file.write_all(&buf).unwrap();
+    }
+    Ok(())
+}
+
+pub fn read_file(file: &mut File, begin_idx: usize, len: usize) -> Vec<u8> {
+    //! 从 file 的 begin_idx 字节开始，读取 len 字节
+    let mut buf = vec![0; len];
+    file.seek(SeekFrom::Start(begin_idx.try_into().unwrap())).unwrap();
+    file.read(&mut buf).unwrap();
+    buf
+}
+
+pub fn to_block(data: &Vec<u8>, len: usize) -> Vec<Vec<u8>> {
+    //! 将一个二级数据块按固定长度 len 分成多个一级数据块
+    let mut res = vec![];
+    for i in (0..data.len()).step_by(len) {
+        res.push(data[i .. i + len].to_vec());
+    }
+    res
+}
+
+pub fn com_block(data: &Vec<Vec<u8>>) -> Vec<u8> {
+    //! 将多个一级数据块合并成一个二级数据块
+    let mut res = vec![];
+    for i in 0..data.len() {
+        for j in 0..data[i].len() {
+            res.push(data[i][j]);
+        }
+    }
+    res
+}
+
+pub fn create_depend(idx_cnt_l: usize, idx_cnt_s: usize, mode_l: usize, mode_s: usize) -> (Vec<Vec<Vec<usize>>>, Vec<Vec<Vec<usize>>>) {
+    //! 创建数据块之间的依赖关系，包括短程依赖和长程依赖
+    let l2_cnt = DATA_L / L2;
+    let l1_cnt = L2 / L1;
+
+    let mut idx_l = vec![];
+    let mut idx_s = vec![];
+
+    for cnt2 in 0..l2_cnt {
+        let mut cur_idx_l = vec![];
+        let mut cur_idx_s = vec![];
+        for cnt1 in 0..l1_cnt {
+            cur_idx_l.push(long_depend(cnt2, idx_cnt_l, mode_l));
+            cur_idx_s.push(short_depend(l1_cnt, cnt1, idx_cnt_s, mode_s));
+        }
+        idx_l.push(cur_idx_l);
+        idx_s.push(cur_idx_s);
+    }
+
+    (idx_l, idx_s)
+}
+
+pub fn vecu8_xor(left: &Vec<u8>, right: &Vec<u8>) -> Vec<u8> {
+    //! 两个vec<u8>逐位异或，结果长度等于 max(left.len(), right.len())
+    let mut res = vec![];
+    let len_left = left.len();
+    let len_right = right.len();
+
+    if len_left > len_right {
+        for i in 0..len_right {
+            res.push(left[i] ^ right[i]);
+        }
+        for j in len_right..len_left {
+            res.push(left[j]);
+        }
+    }
+    else {
+        for i in 0..len_left {
+            res.push(left[i] ^ right[i]);
+        }
+        for j in len_left..len_right {
+            res.push(right[j]);
+        }
+    }
+    res
+}
---
+++ src/proof_of_storage/data_seal.rs
@@ -0,0 +1,87 @@
+use rand::Rng;
+use std::path::PathBuf;
+use std::str::FromStr;
+use std::time::Instant;
+use num_bigint::BigUint;
+
+use super::seal::{copy_and_pad, seal};
+use super::unseal::{unseal, copy_and_compress};
+use super::common::{create_random_file, create_depend};
+
+const ORIGIN_DATA_DIR: [&str; 4] = [r"src", "data_seal", "data", "origin_data"];
+const SEALED_DATA_DIR: [&str; 4] = [r"src", "data_seal", "data", "sealed_data"];
+const UNSEALED_DATA_DIR: [&str; 4] = [r"src", "data_seal", "data", "unsealed_data"];
+
+// 单位：byte
+pub const DATA_L: usize = 16 * 8 * 127;
+pub const L2: usize = 8 * 127;
+pub const L1: usize = 127;
+
+pub const PL2: usize = 8 * 128;
+pub const PL1: usize = 128;
+
+pub const SEAL_ROUNDS: usize = 3;
+pub const MIMC5_HASH_ROUNDS: usize = 110;
+
+pub fn test_deal_seal() {
+    // 随机创建原始数据文件
+    let origin_path: PathBuf = ORIGIN_DATA_DIR.iter().collect();
+    let origin_path = origin_path.to_str().unwrap();
+    create_random_file(origin_path).unwrap();
+    
+    // 创建新的文件，用来存储seal后的数据
+    let sealed_path: PathBuf = SEALED_DATA_DIR.iter().collect();
+    let sealed_path = sealed_path.to_str().unwrap();
+
+    // 创建新的文件，用来存储unseal后的数据
+    let unsealed_path: PathBuf = UNSEALED_DATA_DIR.iter().collect();
+    let unsealed_path = unsealed_path.to_str().unwrap();
+
+    // 长程和短程依赖数据块的个数
+    let idx_cnt_l: usize = 3;
+    let idx_cnt_s: usize = 3;
+
+    // 依赖模式选择
+    let mode_l: usize = 1;
+    let mode_s: usize = 1;
+
+    let mut rng = rand::thread_rng();
+
+    // hash
+    let hash_key = rng.gen();
+    let hash_cts = (0..MIMC5_HASH_ROUNDS)
+        .map(|_| rng.gen())
+        .collect::<Vec<_>>();
+
+    // vde
+    let vde_mode: &str = "sloth";
+    let vde_key = BigUint::from_str("114814770432560997405734776484772649052342276989403295241799079775037429533136378312048423016912262690538326297849170244315795472085749637431992466797211295067626594614696499619970513658328822796782917146952131671224321524616875912503115070404484695218722843806545049037501838881300899940575739494347050015467").unwrap();
+
+    // 生成数据块依赖关系
+    let (idx_l, idx_s) = create_depend(idx_cnt_l, idx_cnt_s, mode_l, mode_s);   
+
+    println!("-------------------------------------");
+
+    // Seal
+    let start = Instant::now();
+    copy_and_pad(origin_path, sealed_path);
+    seal(sealed_path, &idx_l, &idx_s, &hash_cts, hash_key, &vde_key, vde_mode);
+    println!("Seal: {:?}", start.elapsed());
+    // println!("-------------------------------------");
+
+    // Unseal
+    let start = Instant::now();
+    unseal(sealed_path, &idx_l, &idx_s, &hash_cts, hash_key, &vde_key, vde_mode);
+    copy_and_compress(sealed_path, unsealed_path);
+    println!("Unseal: {:?}", start.elapsed());
+    println!("-------------------------------------");
+}
+
+#[test]
+fn test() {
+    const SAMPLES: usize = 1;
+    for i in 0..SAMPLES {
+        println!("Sample: {:?}", i);
+        test_deal_seal();
+    }
+}
